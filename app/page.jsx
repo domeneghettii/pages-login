c36cb6da736665c901624553096a2b9e1639707b
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2025-03-13 09:27:18 -0300

    feat: update page

--- app/page.jsx
+++ app/page.jsx
@@ -1,33 +1,30 @@
+import React from 'react';
 
 export default function Home() {
     return (
-
-        <div class="container">
+        <div className="container">
             <h1>Welcome!</h1>
             <h2>Login</h2>
-            <p class="text-sm text-center mb-10">Don't have an account? <a href="https://www.seedprod.com">Get SeedProd Now</a></p>
+            <p className="text-sm text-center mb-10">Don't have an account? <a href="/SignUp">Sign Up</a></p>
 
             <div className="card-login">
-            <h3>Cadastra-se</h3>
-            <input type="text" id="taskInput" placeholder='Username'/>
-            <input type="password" id="taskInput" placeholder='Password'/>
-            <input type="password" id="taskInput" placeholder='Confirm Password'/>
-            <button onclick="addTask()">Cadastrar</button>
+                <h3>Cadastra-se</h3>
+                <input type="text" placeholder='Username'/>
+                <input type="password" placeholder='Password'/>
+                <input type="password" placeholder='Confirm Password'/>
+                <button onClick={() => alert('Register function not implemented')}>Cadastrar</button>
 
-            <h3>Login</h3>
-            <input type="text" id="taskInput" placeholder='Username'/> 
-            <input type="password" id="taskInput" placeholder='Password'/>
-            <button onclick="addTask()">Login</button> 
+                <h3>Login</h3>
+                <input type="text" placeholder='Username'/>
+                <input type="password" placeholder='Password'/>
+                <button onClick={() => alert('Login function not implemented')}>Login</button>
             </div>
 
-            <div class="icon">
-            <a href="https://www.instagram.com"><img src="https://img.icons8.com/ios-filled/50/000000/instagram-new.png"/></a>
-            <a href="https://www.twitter.com"><img src="https://img.icons8.com/ios-filled/50/000000/twitter.png"/></a>
-            <a href="https://www.linkedin.com"><img src="https://img.icons8.com/ios-filled/50/000000/linkedin.png"/></a>
-
-                
-                </div>
+            <div className="icon">
+                <a href="https://www.instagram.com"><img src="https://img.icons8.com/ios-filled/50/000000/instagram-new.png" alt="Instagram"/></a>
+                <a href="https://www.twitter.com"><img src="https://img.icons8.com/ios-filled/50/000000/twitter.png" alt="Twitter"/></a>
+                <a href="https://www.linkedin.com"><img src="https://img.icons8.com/ios-filled/50/000000/linkedin.png" alt="LinkedIn"/></a>
+            </div>
         </div>
-    )
+    );
 }
-
---
+++ layout.js
@@ -0,0 +1,11 @@
+import "../styles/globals.css";
+			export const metadata = {
+				title: "Meu App",
+			};
+			export default function RootLayout({ children }) {
+				return (
+					<html>
+						<body>{children}</body>
+					</html>
+);
+		}
